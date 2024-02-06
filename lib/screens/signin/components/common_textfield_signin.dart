63d9c7dea4cedea66e0e9be9d785ea50d8be2bdf
Nikunj-Trueline <nikunjtrueline@gmail.com>
2024-02-06 15:02:15 +0530

    sign in screen add.

--- lib/screens/signin/components/common_textfield_signin.dart
+++ lib/screens/signin/components/common_textfield_signin.dart
@@ -36,37 +36,3 @@ TextFormField commonTextFieldForSignInField({
 
 // This Is Common textField For signIn Screen.
 
-
-TextFormField commonTextFieldForSignInField1({
-  String? hintText,
-  void Function()? onTap,
-  IconData? iconForSuffix,
-  TextEditingController? controller,
-  required bool status,
-}) {
-  return TextFormField(
-    cursorHeight: 32,
-    cursorWidth: 3,
-    controller: controller,
-    cursorColor: Colors.amber.shade700,
-    onTap: onTap,
-    obscureText: status,
-    // style: textStyleForTextFormField(),
-    decoration: InputDecoration(
-      suffixIcon: IconButton(
-        icon: Icon(
-          iconForSuffix,
-          size: 35,
-        ),
-        onPressed: onTap,
-        padding: const EdgeInsets.all(10),
-      ),
-      enabledBorder: const UnderlineInputBorder(
-        borderSide: BorderSide(color: Colors.grey),
-      ),
-      focusedBorder: UnderlineInputBorder(
-        borderSide: BorderSide(color: Colors.amber.shade700),
-      ),
-    ),
-  );
-}
