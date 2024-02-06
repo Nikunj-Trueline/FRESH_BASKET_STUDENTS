ccbefaaede9c2088990ce988ab1f8c6e17c6781a
Nikunj-Trueline <nikunjtrueline@gmail.com>
2024-02-06 10:28:01 +0530

    sign in screen add.

--- lib/screens/signin/signin_screen.dart
+++ lib/screens/signin/signin_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'components/signin_body.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const SignInBody();
   }
 }
